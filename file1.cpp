d3b49b67fcf08806b89c5a7c5de6218050f34c25
yousra <aliyousra786@gmail.com>
2025-11-13 11:13:18 +0530

    first commit done

--- file1.cpp
+++ file1.cpp
@@ -161,7 +161,7 @@ int main(){
     //     }
     //     cout<<endl;
     // }
-
+      cout<<"Learning binary search"<<endl;
     for(int i=1; i<=n; i++){
         for(int j=1; j<n-i+2; j++){
             cout<<j<<" ";
